6f217eb116b5b11fe4ed70a33d78cd09031e6d65
Simone Padula <simone.padula@itsincom.it>
2025-01-27 14:15:14 +0100

    Basic Project Structure Creation

---
+++ pw5-frontend/src/app/pages/single-company/single-company.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-company',
+  imports: [],
+  templateUrl: './single-company.component.html',
+  styleUrl: './single-company.component.css'
+})
+export class SingleCompanyComponent {
+
+}
---
+++ pw5-frontend/src/app/pages/user-area/user-area.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-area',
+  imports: [],
+  templateUrl: './user-area.component.html',
+  styleUrl: './user-area.component.css'
+})
+export class UserAreaComponent {
+
+}
