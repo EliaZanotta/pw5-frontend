3433112b2114169ca68810e5ce32f0e84e93f3dd
Simone <simone.russo@itsincom.it>
2025-01-31 14:14:04 +0100

    events page connected to database

--- pw5-frontend/src/app/pages/events/events.component.ts
+++ pw5-frontend/src/app/pages/events/events.component.ts
@@ -1,7 +1,20 @@
-import {Component} from '@angular/core';
+import {EventsService} from './events.service';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {DatePipe, NgForOf} from '@angular/common';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEuroSign } from '@fortawesome/free-solid-svg-icons';
 
+interface Event {
+  id: number;
+  title: string;
+  startDate: string;
+  endDate: string;
+  place: string;
+  host: string;
+  eventSubscription: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-events',
@@ -10,29 +23,35 @@ import {DatePipe, NgForOf} from '@angular/common';
   imports: [
     RouterLink,
     NgForOf,
-    DatePipe
+    DatePipe,
+    NgIf,
+    FontAwesomeModule
   ],
   standalone: true
 })
-export class EventsComponent {
-  eventCategories = [
-    {
-      title: 'Upcoming Events',
-      events: [
-        {id: 1, title: 'Tech Conference 2023', date: '2023-09-15', image: 'img/code_cheers.png'},
-        {id: 2, title: 'AI Summit', date: '2023-10-01', image: 'img/code_cheers.png'},
-        {id: 3, title: 'Web Dev Bootcamp', date: '2023-10-15', image: 'img/code_cheers.png'},
-        {id: 4, title: 'Startup Pitch Night', date: '2023-11-01', image: 'img/code_cheers.png'},
-      ]
-    },
-    {
-      title: 'Past Events',
-      events: [
-        {id: 5, title: 'Data Science Workshop', date: '2023-05-20', image: 'img/code_cheers.png'},
-        {id: 6, title: 'UX Design Seminar', date: '2023-06-10', image: 'img/code_cheers.png'},
-        {id: 7, title: 'Blockchain Symposium', date: '2023-07-05', image: 'img/code_cheers.png'},
-        {id: 8, title: 'Mobile App Hackathon', date: '2023-08-01', image: 'img/code_cheers.png'},
-      ]
-    }
-  ];
+
+export class EventsComponent implements OnInit {
+  faEuroSign = faEuroSign;
+
+  eventsByCategory: { [key: string]: Event[] } = {
+    future: [],
+    past: []
+  };
+
+  constructor(private eventsService: EventsService) {
+  }
+
+  ngOnInit(): void {
+    this.eventsService.getEvents().subscribe((events: Event[]) => {
+      this.eventsByCategory = this.categorizeEvents(events);
+    });
+  }
+
+  private categorizeEvents(events: Event[]): { future: Event[]; past: Event[] } {
+    const futureEvents: Event[] = events.filter(event => event.status === 'CONFIRMED');
+    const pastEvents: Event[] = events.filter(event => event.status === 'ARCHIVED');
+
+    return {future: futureEvents, past: pastEvents};
+  }
 }
+
