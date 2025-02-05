84509688fb7a18ede054bfebe4d92fdaebf4fe9d
Elia <elia.zanotta@itsincom.it>
2025-02-05 10:38:11 +0100

    Fix speaker-cta

--- pw5-frontend/src/app/components/speaker-cta/speaker-cta.component.ts
+++ pw5-frontend/src/app/components/speaker-cta/speaker-cta.component.ts
@@ -1,8 +1,9 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthService, User } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-speaker-cta',
@@ -10,11 +11,34 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './speaker-cta.component.html',
   styleUrl: './speaker-cta.component.css'
 })
-export class SpeakerCtaComponent {
-  isLoggedIn = true; // This should be dynamically set based on your authentication logic
+export class SpeakerCtaComponent implements OnInit {
+  user: User | null = null;
+  isLoggedIn: boolean = false;
+  isUser: boolean = false;
+
+  constructor(private authService: AuthService) { }
+
+  async ngOnInit(): Promise<void> {
+    await this.loadUser();
+  }
+
+  private async loadUser(): Promise<void> {
+    try {
+      const userResult = (await this.authService.getLoggedUser()).user;
+      if (userResult && userResult.id && userResult.role) {
+        this.user = userResult;
+        this.isLoggedIn = true;
+        this.isUser = userResult.role.toUpperCase() === 'USER';
+      }
+    } catch (error) {
+      console.error('Errore nel recupero dell\'utente:', error);
+      this.user = null;
+      this.isLoggedIn = false;
+      this.isUser = false;
+    }
+  }
 
   navigateToSpeakerApplication() {
-    // Implement navigation logic here
     console.log('Navigating to speaker application page');
   }
 }
